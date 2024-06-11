85375f653ef62ae12084159d9f9bd64eec70f61b
dave <okxgarcia@gmail.com>
2024-06-10 23:47:51 -0500

    ok

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.secret_key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 #app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:123456@localhost:5432/test'
 #engine=create_engine('postgresql://ok:7qF6RmXo5vKNJ4QzWOQDD4NyPGigclw9@dpg-cpgajvmct0pc73dag83g-a:5432/db1_dhy6')
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://ok:b5Dko0Kgdnn3L7Oj6Qpl05Lz0rTopmSc@dpg-cphs6eq1hbls73b8isk0-a:5432/db1_oi4n"
-#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/bahia3"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://ok:b5Dko0Kgdnn3L7Oj6Qpl05Lz0rTopmSc@dpg-cphs6eq1hbls73b8isk0-a:5432/db1_oi4n"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/bahia2"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
@@ -33,16 +33,16 @@ except:
     pass
 
 if __name__ == '__main__':
-    #csrf = CSRFProtect(app)
-    app.run(host='0.0.0.0', debug=True)
-    #app.run(debug=True)
+    csrf = CSRFProtect(app)
+    #app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
 #ImportError: cannot import name 'app_blueprint1' from partially initialized module 'fun_inicio' (most likely due to a circular import) 
 #<a href="/perfil">
 #    {{ session['nombre'] }}
 #    <!--<img style="width:50px;height:30px;" src="{{url_for('static',filename='images/logos-iconos/perfil1.jpg')}}">-->
 #</a>
 
-#@media screen and (min-width:800px) {
+##@media screen and (min-width:800px) {
 #            .titulo{font-size: 18px;justify-content: flex-end}
 #            .parrafo_publicidad1 p{font-size:16px;}
 #            .cont_nav a{margin: 0px 4px;}
